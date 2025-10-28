96b828941691ca23c3c72ac7011a0063d38c97f9
ferdoushasanpr <ferdoushasanpr@gmail.com>
2025-10-28 22:12:12 +0600

    feat(add): Form Added to New Item Screen.

--- lib/screens/new_item.dart
+++ lib/screens/new_item.dart
@@ -1,13 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:grocerystore/data/categories.dart';
 
-class NewItem extends StatelessWidget {
+class NewItem extends StatefulWidget {
   const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() => _NewItemState();
+}
+
+class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<FormState>();
+
+  void _saveForm() {
+    _formKey.currentState!.validate();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Add New Item")),
-      body: Center(child: Text("New Item Screen")),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 20,
+                decoration: InputDecoration(label: Text("Item Name")),
+                validator: (value) {
+                  if (value == null ||
+                      value.isEmpty ||
+                      value.trim().length <= 1) {
+                    return "Must be at least 2 characters long.";
+                  }
+
+                  return null;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: "1",
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(label: Text("Quantity")),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      value: categories.values.first.title,
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value.title,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 24,
+                                  height: 24,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      _formKey.currentState!.reset();
+                    },
+                    child: Text("Reset"),
+                  ),
+                  ElevatedButton(onPressed: _saveForm, child: Text("Save")),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
